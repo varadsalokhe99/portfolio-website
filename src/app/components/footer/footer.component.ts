0a854b0cedb62728e2909d94d4241d5de14000b4
varadsalokhe99 <salokhevarad99@gmail.com>
2025-03-05 04:03:58 +0530

    completing footer content and adding styles to globel file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  skills = [
+    { name: 'HTML', value: 90 },
+    { name: 'CSS', value: 65 },
+    { name: 'React', value: 70 },
+    { name: 'JS', value: 75 },
+    { name: 'Angular', value: 75 },
+    { name: 'TS', value: 80 },
+  ];
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const progressBars = document.querySelectorAll('.progress-bar');
+      progressBars.forEach((bar: any, index: number) => {
+        bar.style.width = this.skills[index].value + '%';
+      });
+    }, 500); 
+  }
 
 }
