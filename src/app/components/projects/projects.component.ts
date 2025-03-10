5653781e599b23c3971cfbffc01ed23aff626232
varadsalokhe99 <salokhevarad99@gmail.com>
2025-03-10 20:26:22 +0530

    adding projects in project section and modify footer component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'Custom Business Website for Four Principals Solutions',
+      subtitle:
+        'A fully customized business website built with Angular & PrimeNG',
+      description:
+        'Developed and deployed a fully customized business website for Four Principals Solutions, designed to enhance its online presence. The project was built using Angular, TypeScript, and PrimeNG, ensuring a responsive and user-friendly experience. Integrated EmailJS for seamless contact form functionality.',
+      techStack: ['Angular', 'TypeScript', 'PrimeNG', 'EmailJS'],
+      liveDemo: '',
+      gitRepo: '',
+      thumbnail: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Custom Business Website for Four Principals Solutions',
+      subtitle:
+        'A fully customized business website built with Angular & PrimeNG',
+      description:
+        'Developed and deployed a fully customized business website for Four Principals Solutions, designed to enhance its online presence. The project was built using Angular, TypeScript, and PrimeNG, ensuring a responsive and user-friendly experience. Integrated EmailJS for seamless contact form functionality.',
+      techStack: ['Angular', 'TypeScript', 'PrimeNG', 'EmailJS'],
+      liveDemo: '',
+      gitRepo: '',
+      thumbnail: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Custom Business Website for Four Principals Solutions',
+      subtitle:
+        'A fully customized business website built with Angular & PrimeNG',
+      description:
+        'Developed and deployed a fully customized business website for Four Principals Solutions, designed to enhance its online presence. The project was built using Angular, TypeScript, and PrimeNG, ensuring a responsive and user-friendly experience. Integrated EmailJS for seamless contact form functionality.',
+      techStack: ['Angular', 'TypeScript', 'PrimeNG', 'EmailJS'],
+      liveDemo: '',
+      gitRepo: '',
+      thumbnail: 'https://via.placeholder.com/150',
+    },
+  ];
 }
